c89e2cef42fd1cb1fb80f5e6ab42e468b47a14ff
ZiniFili <101247036+Zifili@users.noreply.github.com>
2024-01-31 19:27:50 +0100

    Update GridArticle.jsx

--- sito_bello/src/shop/GridArticle.jsx
+++ sito_bello/src/shop/GridArticle.jsx
@@ -76,11 +76,35 @@ function Card({ src, cost, title, description }) {
     </div>
   );
 }
+function SidebarElement({value}){
+  return (
+    <li>
+            <a
+              href="#"
+              class="flex items-center p-2 rounded-lg  hover:bg-blackk group"
+            >
+              <svg
+                class="w-5 h-5 transition duration-75 group-hover:text-blackk dark:group-hover:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 21"
+              >
+                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+              </svg>
+              <span class="ms-3">Dashboard</span>
+            </a>
+          </li>
+  );
+}
 function Sidebar() {
   return (
-    <aside>
-      <div>
-        piero angela
+    <aside className="text-white">
+      <div class="h-full px-20 py-4">
+        <ul class="space-y-2 font-medium">
+          <SidebarElement/>
+        </ul>
       </div>
     </aside>
   );
@@ -107,35 +131,36 @@ export default function GridArticle() {
         <div class="lg:grid grid-rows-2 lg:grid-flow-col lg:gap-4 text-white ">
           <Card
             src="../src/assets/img4.JPG"
-            title="cock"
-            description="cock"
+            title="product"
+            description="product"
             cost="$20"
           />
           <Card
             src="../src/assets/img4.JPG"
-            title="cock"
-            description="cock"
+            title="product"
+            description="product"
             cost="$20"
           />
           <Card
             src="../src/assets/img4.JPG"
-            title="cock"
-            description="cock"
+            title="product"
+            description="product"
             cost="$20"
           />
           <Card
             src="../src/assets/img4.JPG"
-            title="cock"
-            description="cock"
+            title="product"
+            description="product"
             cost="$20"
           />
           <Card
             src="../src/assets/img4.JPG"
-            title="cock"
-            description="cock"
+            title="product"
+            description="product"
             cost="$20"
           />
         </div>
+        {/*CARD SHOP*/}
       </div>
     </>
   );
