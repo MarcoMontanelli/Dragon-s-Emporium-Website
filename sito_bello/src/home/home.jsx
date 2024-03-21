af2f8ddb580878078f35542fddcd9b88e5422e85
ZiniFili <101247036+Zifili@users.noreply.github.com>
2024-03-21 22:45:23 +0100

    dwa

--- sito_bello/src/home/home.jsx
+++ sito_bello/src/home/home.jsx
@@ -5,12 +5,14 @@ import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import Jumbo from "./Jumbo.jsx";
 import Video from "./Video.jsx";
+import { SparklesPreview } from "./Test_sparkle.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <Jumbo />
     <Video /> 
+    <SparklesPreview />
     <Footer />
   </React.StrictMode>
 );
